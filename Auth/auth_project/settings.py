4f54d8ad060276041e2d6a2d002b7c9c43a00fcc
ruff1376 <ruff1376@gmail.com>
2025-06-11 12:53:19 +0900

    Auth 회원가입, 로그인, 로그아웃

--- Auth/auth_project/settings.py
+++ Auth/auth_project/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+AUTH_USER_MODEL = 'accounts.CustomUser' # 커스텀 유저 모델 설정
--- Auth/post/models.py
+++ Auth/post/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
